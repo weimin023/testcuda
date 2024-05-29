d6b2aef5898755eb098ce5bff1d3b5b8e6d8674b
weimin.chen <weimin.chen@tronfuture.com>
2024-05-29 13:21:41 +0800

    update

--- unittest.cu
+++ unittest.cu
@@ -1,10 +1,15 @@
 #include <gtest/gtest.h>
+#include <thrust/device_vector.h>
 #include "test_cuda.cu"
 
 TEST(CUDAfunction, test_cuMath_vec) {
     cuMath::cuVec v(7, 2);
     auto a = v[0];
+
+    thrust::device_vector<int> vec(10, 77);
+
     EXPECT_EQ(a, 2);
+    EXPECT_EQ(vec[0], 77);
 
 }
 
