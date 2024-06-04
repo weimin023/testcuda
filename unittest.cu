a9e25f2f9e9ec7c883c818166d50b90c86f34711
weimin.chen <weimin.chen@tronfuture.com>
2024-06-04 10:23:11 +0800

    update

--- unittest.cu
+++ unittest.cu
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 #include <thrust/device_vector.h>
 #include "test_cuda.cu"
+#include "test_fft.cpp"
 
-TEST(CUDAfunction, test_cuMath_vec) {
+/*TEST(CUDAfunction, test_cuMath_vec) {
     cuMath::cuVec v(7, 2);
     auto a = v[0];
 
@@ -10,7 +11,17 @@ TEST(CUDAfunction, test_cuMath_vec) {
 
     EXPECT_EQ(a, 2);
     EXPECT_EQ(vec[0], 77);
+}*/
 
+TEST(nsight, fft) {
+    testFFT fft;
+    std::vector<std::complex<double>> data = {
+        {1, 0}, {7, 0}, {8, 0}, {2, 0},
+        {1, 0}, {2, 0}, {8, 0}, {2, 0},
+        {3, 0}, {4, 0}, {2, 0}, {8, 0},
+        {9, 0}, {8, 0}, {1, 0}, {2, 0}
+    };
+    fft.run(data);
 }
 
 int main(int argc, char **argv) {
