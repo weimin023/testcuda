6b6b9b87a65c06ffbb756e48c41b034be7b342a8
weimin.chen <weimin.chen@tronfuture.com>
2024-09-08 16:35:51 +0800

    update

---
+++ gemm_comm.cu
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+double get_walltime()
+{
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return (double)(tp.tv_sec + tp.tv_usec * 1e-6);
+}
+
+void matrixSerial(float *hostA, float *hostB, float *hostC, int M, int K, int N)
+{
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            float tmp = 0;
+            for (int s = 0; s < K; s++)
+            {
+                tmp += hostA[i * K + s] * hostB[s * N + j];
+            }
+            hostC[i * N + j] = tmp;
+        }
+    }
+}
+
+void compare(float *hostC, float *serialC, int M, int N)
+{
+    float error = 0;
+    bool tmp = true;
+    for (int i = 0; i < M * N; i++)
+    {
+        error = fmax(error, fabs(hostC[i] - serialC[i]));
+        if (error > 1e-2)
+        {
+            tmp = false;
+            printf("error:hostC[%d] = %.3f, serialC[%d] = %.3f\n", i, hostC[i], i, serialC[i]);
+            break;
+        }
+    }
+    if (tmp)
+    {
+        printf("GPU output all right\n");
+    }
+}
--- gemm_naive.cu
+++ gemm_naive.cu
@@ -4,33 +4,70 @@
 #include <random>
 #include <vector>
 #include <cassert>
+#include "gemm_comm.cu"
 
 // Naive GEMM
-
-__global__ void NaiveGEMM(int *in_A, int *in_B, int *out_C, int M, int N, int K) {
+__global__ void gemm_naive(float *dA, float *dB, float *dC, int M, int K, int N)
+{
     int row = threadIdx.x + blockIdx.x * blockDim.x;
     int col = threadIdx.y + blockIdx.y * blockDim.y;
-
-    if (row >= M || col >= N) return;
-
-    int s = 0.0;
-    for (int k=0;k<K;++k) {
-        s += in_A[row * K + k] * in_B[k * N + col];
+    float tmp = 0;
+    if (row < M && col < N)
+    {
+        for (int s = 0; s < K; s++)
+        {
+            tmp += dA[row * K + s] * dB[s * N + col];
+        }
+        dC[row * N + col] = tmp;
     }
-    out_C[row * N + col] = s;
+}
 
+// shared mem with tile
+template<int TILE_SIZE> __global__ void gemm_shared(float *dA, float *dB, float *dC, int M, int K, int N) {
+    int row = threadIdx.x + blockIdx.x * blockDim.x;
+    int col = threadIdx.y + blockIdx.y * blockDim.y;
+    int width = (K + TILE_SIZE - 1) / TILE_SIZE;
+
+    __shared__ float SA[TILE_SIZE][TILE_SIZE];
+    __shared__ float SB[TILE_SIZE][TILE_SIZE];
+    float reg_tmp = 0;
+
+    for (int w=0;w<width;++w) {
+        if (row < M && (w * TILE_SIZE + threadIdx.y) < K) {
+            SA[threadIdx.x][threadIdx.y] = dA[row * K + w * TILE_SIZE + threadIdx.y];
+        } else {
+            SA[threadIdx.x][threadIdx.y] = 0;
+        }
+        if (col < N && (w * TILE_SIZE + threadIdx.x) < K) {
+            SB[threadIdx.x][threadIdx.y] = dB[(w * TILE_SIZE + threadIdx.x)*N + col];
+        } else {
+            SB[threadIdx.x][threadIdx.y] = 0;
+        }
+        __syncthreads();
+
+        for (int s=0;s<TILE_SIZE;++s) {
+            reg_tmp += SA[threadIdx.x][s] * SB[s][threadIdx.y];
+        }
+        __syncthreads();
+    }
+    if (row < M && col < N) {
+        dC[row * N + col] = reg_tmp;
+    }
+    
 }
 
 int main() {
     
-    int m = 8;
-    int n = 8;
-    int k = 8;
+    int m = 1024;
+    int n = 1024;
+    int k = 1024;
+
+    int trials = 100;
 
     int matrixSize = m * n;
 
-    thrust::host_vector<int> h_A(matrixSize);
-    thrust::host_vector<int> h_B(matrixSize);
+    thrust::host_vector<float> h_A(matrixSize);
+    thrust::host_vector<float> h_B(matrixSize);
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -41,65 +78,47 @@ int main() {
         h_B[i] = dis(gen);
     }
 
-    thrust::device_vector<int> d_A = h_A;
-    thrust::device_vector<int> d_B = h_B;
-    thrust::device_vector<int> d_C(matrixSize, 0.0);
+    thrust::device_vector<float> d_A = h_A;
+    thrust::device_vector<float> d_B = h_B;
+    thrust::device_vector<float> d_C(matrixSize, 0.0);
 
-    dim3 threadNum(16, 16);
+    dim3 threadNum(32, 32);
     dim3 blockNum((m + threadNum.x - 1)/threadNum.x, (n + threadNum.y - 1)/threadNum.y);
-    NaiveGEMM<<<blockNum, threadNum>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, n, 8);
 
-    thrust::host_vector<int> h_C = d_C;
+    float ker_time = 0;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
 
-    std::cout << "A_in: " << std::endl;
-    for (int i=0;i<m;++i) {
-        for (int j=0;j<n;++j) {
-            int idx = i * n + j;
-            std::cout << h_A[idx] << ", ";
-            if ((idx+1)%m == 0) std::cout << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    std::cout << "B_in: " << std::endl;
-    for (int i=0;i<m;++i) {
-        for (int j=0;j<n;++j) {
-            int idx = i * n + j;
-            std::cout << h_B[idx] << ", ";
-            if ((idx+1)%m == 0) std::cout << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    std::cout << "C_out: " << std::endl;
-    for (int i=0;i<m;++i) {
-        for (int j=0;j<n;++j) {
-            int idx = i * n + j;
-            std::cout << h_C[idx] << ", ";
-            if ((idx+1)%m == 0) std::cout << std::endl;
-        }
-    }
-    std::cout << std::endl;
-
-    std::vector<int> cpu_c(m*n, 0);
-    for (int i=0;i<m;++i) {
-        for (int j=0;j<n;++j) {
-            int idx = i * n + j;
-            for (int c=0;c<k;++c) {
-                cpu_c[idx] += h_A[i * k + c] * h_B[j + c * n];
-            }
-        }
+    for (int i=0;i<trials;++i) {
+        // gemm_naive<<<blockNum, threadNum>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, k, n);
+        gemm_shared<32><<<blockNum, threadNum>>>(d_A.data().get(), d_B.data().get(), d_C.data().get(), m, k, n);
     }
 
-    bool flg = 1;
-    for (int i=0;i<m*n;++i) {
-        if (cpu_c[i] != h_C[i]) {
-            std::cout << "CPU and GPU results NOT match!" << std::endl;
-            flg = 0;
-            break;
-        }
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
     }
-    if (flg) std::cout << "SUCCESS: CPU and GPU results match!" << std::endl;
+
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&ker_time, start, stop);
+    printf("kernel time: %.4f second, %.4f ms\n", ker_time / (trials * 1000.), ker_time / trials);
+    printf("grid dim: %d, %d, %d\n", blockNum.x, blockNum.y, blockNum.z);
+    printf("block dim: %d, %d, %d\n", threadNum.x, threadNum.y, threadNum.z);
+
+    thrust::host_vector<float> h_C = d_C;
+
+    std::vector<float> cpu_v(m*n);
+    double st, ela;
+    st = get_walltime();
+    matrixSerial(h_A.data(), h_B.data(), cpu_v.data(), m, k, n);
+    ela = get_walltime() - st;
+    printf("CPU time:%.2f second\n", ela);
+
+    compare(h_C.data(), cpu_v.data(), m, n);
     
     return 0;
 }
